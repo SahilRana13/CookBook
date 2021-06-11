d01f671968762778a37375d6a06a6ab6189a6c75
Harish babu alle <harishbabualle@gmail.com>
2021-06-11 15:15:44 -0400

    Animation applied to the splash screen

--- app/src/main/java/com/example/cookbook/SplashScreen.java
+++ app/src/main/java/com/example/cookbook/SplashScreen.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
 
+    ImageView iconImage;
+    Animation animation1;
     private final int SPLASH_DISPLAY_LENGTH = 3000;
 
 
@@ -16,6 +21,11 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        iconImage = findViewById(R.id.iconImage);
+        animation1 = AnimationUtils.loadAnimation(SplashScreen.this,R.anim.zoonin);
+
+        iconImage.startAnimation(animation1);
 
         new Handler().postDelayed(new Runnable(){
             @Override
