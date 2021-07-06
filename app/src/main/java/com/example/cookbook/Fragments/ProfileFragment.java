a94400cc03808697b68a9fadc0ed51e5948cde8f
kallisaikiran <kalli.saikiran@outlook.com>
2021-07-05 20:40:37 -0400

    User Profile Data retrived successfully

--- app/src/main/java/com/example/cookbook/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/cookbook/Fragments/ProfileFragment.java
@@ -7,16 +7,41 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.cookbook.R;
-
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
 
 
 public class ProfileFragment extends Fragment {
+
+    private TextView email,editTool;
+    private Button saveButton;
+    private EditText editName;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore db;
+    private ImageView dp;
+
+    private NavController navController;
+    private FirebaseUser currentUser;
+    private FirebaseStorage firebaseStorage;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -36,5 +61,48 @@ public class ProfileFragment extends Fragment {
 
         getActivity().setTitle("Profile");
 
+        dp = view.findViewById(R.id.ProfileImage);
+        editTool = view.findViewById(R.id.editNameTool);
+        saveButton = view.findViewById(R.id.SaveBtn);
+        editName = view.findViewById(R.id.EditProfileName);
+        email = view.findViewById(R.id.ProfileEmail);
+        db = FirebaseFirestore.getInstance();
+
+        navController = Navigation.findNavController(getActivity(),R.id.Host_Fragment2);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        firebaseStorage = FirebaseStorage.getInstance();
+
+        getData();
+
+    }
+
+    private void getData() {
+        currentUser = firebaseAuth.getCurrentUser();
+
+        DocumentReference docRef = db.collection("User Profile Information").document(currentUser.getUid());
+
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                if (documentSnapshot.exists())
+                {
+                    String resultEmail = documentSnapshot.getString("email");
+                    String resultName = documentSnapshot.getString("name");
+
+                    email.setText(resultEmail);
+                    editName.setText(resultName);
+                }
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                Toast.makeText(getActivity(), "Something Wrong", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
